f22d1a60257aae1dab50360d92f7106589ff3da2
Lucas Costa <luchtml5@gmail.com>
2022-12-15 21:33:35 -0300

    all

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const menu = {
-  class: '.principal',
-  ativar(){
-    const menuElement = document.querySelector(this.class)
-    console.log(menuElement)
+// const menu = {
+//   class: '.principal',
+//   ativar(){
+//     const menuElement = document.querySelector(this.class)
+//     console.log(menuElement)
+//   }
+// }
+
+// menu.ativar()
+
+// function upperName(name) {
+//   return name.toUpperCase()
+// }
+
+class Menu {
+  constructor(menu) {
+    this.menuElement = document.querySelector(menu)
+    this.activeClass = 'active'
+  }
+  addActiveEvent() {
+    this.menuElement.addEventListener('click', function (event) {
+      event.target.classList.add(this.activeClass)
+    })
   }
 }
 
-menu.ativar()
+const menu = new Menu ('.principal')
+menu.addActiveEvent()
+console.log(menu)
