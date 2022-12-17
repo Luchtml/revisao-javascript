473e4e9151ed0050dd58aee71e84504ea8cb55fd
Lucas Costa <luchtml5@gmail.com>
2022-12-17 14:39:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,38 @@
 //   return name.toUpperCase()
 // }
 
-class Menu {
-  constructor(menu) {
-    this.menuElement = document.querySelector(menu);
-    this.activeClass = 'active';
-  }
-  addActiveEvent() {
-    this.menuElement.addEventListener('click',  (event) => {
-      event.target.classList.add(this.activeClass);
-    console.log(this);
+// class Menu {
+//   constructor(menu) {
+//     this.menuElement = document.querySelector(menu);
+//     this.activeClass = 'active';
+//   }
+//   addActiveEvent() {
+//     this.menuElement.addEventListener('click',  (event) => {
+//       event.target.classList.add(this.activeClass);
+//     console.log(this);
 
-    });
-  }
-}
+//     });
+//   }
+// }
 
-const menu = new Menu('.principal');
-menu.addActiveEvent();
+// const menu = new Menu('.principal');
+// menu.addActiveEvent();
+
+// function handleMouseMove(event) {
+//   const { clientX, clientY, target } = event;
+//   console.log(clientX, clientY);
+// }
+
+// const frutas = ['Banana', 'Uva']
+
+// document.documentElement.addEventListener('mousemove', handleMouseMove);
+// function showList(empresa, ...clientes) {
+//   console.log(empresa)
+//   console.log(clientes)
+// }
+
+// showList('Lucas', 'Joa', 'Thamires', 10)
+
+const numeros = [1,2,3,4,5,6,7,8,9]
+
+console.log(Math.max(...numeros))
