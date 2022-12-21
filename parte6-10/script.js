e2249006f469efe1061dc962131effbb3569c1d7
Lucas Costa <luchtml5@gmail.com>
2022-12-21 12:08:15 -0300

    Update script.js

--- parte6-10/script.js
+++ parte6-10/script.js
@@ -11,3 +11,29 @@
 
 // Arrays metodos
 
+const precos = [
+  "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus dados"
+];
+
+const precosFiltro = precos.filter((preco) => {
+return preco.includes('R$')
+})
+
+const precosNumeros = precosFiltro.map((preco) => {
+  console.log(preco)
+  return +preco.replace("R$ ", "")
+})
+
+console.log(precosNumeros)
+
+
+const total = precosNumeros.reduce((acc, item) => acc + item);
+
+
+console.log(total)
