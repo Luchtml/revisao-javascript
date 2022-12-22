f496255f52226f2346660a718d82fe46f98b11de
Lucas Costa <luchtml5@gmail.com>
2022-12-22 14:04:56 -0300

    Update script.js

--- classes/script.js
+++ classes/script.js
@@ -1,9 +1,22 @@
 class SmoothScroll {
   constructor(links) {
-    this.linkElements = document.querySelectorAll(links)
+    this.linkElements = document.querySelectorAll(links);
+    this.addClickEvent()
+  }
+  
+  handleClick(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+  }
+
+  addClickEvent() {
+    this.linkElements.forEach((link) => {
+      link.addEventListener('click', this.handleClick)
+    })
   }
 }
 
-const scroll = new SmoothScroll("a")
+const scroll = new SmoothScroll("a[href^='#']");
 
-console.log(scroll.linkElements)
+console.log(scroll.linkElements);
